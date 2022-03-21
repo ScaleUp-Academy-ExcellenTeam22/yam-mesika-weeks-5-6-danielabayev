bb14c58174966081cdb26c1a11b257e563d580b5
daniel or abayev <dan5204@gmail.com>
2022-03-21 16:59:59 +0200

    resolve all the pull request reviewe, except the return type

--- this_the_way.py
+++ this_the_way.py
@@ -1,12 +1,13 @@
 import os
 
 
-# the function read the path and create list of the files start with deep, then print them
 def thisTheWay():
+    # the function read the path and create list of the files start with deep, then print them
     folder_path = input("insert the path to the folder")
     directory_list = os.listdir(folder_path)
-    only_deep = [f for f in directory_list if f.startswith("deep")]
-    print(only_deep)
+    start_with_deep_files = [file_names for file_names in directory_list if file_names.startswith("deep")]
+    return start_with_deep_files
 
 
-thisTheWay()
+files_start_deep = thisTheWay()
+print(files_start_deep)
