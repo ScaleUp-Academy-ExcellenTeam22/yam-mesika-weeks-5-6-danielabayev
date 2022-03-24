d15e56ef622119ed164e9ad2c93d543aeebb6f45
daniel or abayev <dan5204@gmail.com>
2022-03-24 15:53:12 +0200

    fix the fix documentation and add main check

--- this_the_way.py
+++ this_the_way.py
@@ -3,8 +3,8 @@ import os
 
 def this_the_way() -> list[str]:
     """
-    This function get from the user path to folder and return all the files start with the word "deep"
-    :return: list of the files start with deep
+    This function get from the user path to folder and return all the files start with the word "deep".
+    :return: list of the files start with deep.
     """
     # the function read the path and create list of the files start with deep, then print them
     folder_path = input("insert the path to the folder")
@@ -13,5 +13,6 @@ def this_the_way() -> list[str]:
     return start_with_deep_files
 
 
-files_start_deep = this_the_way()
-print(files_start_deep)
+if __name__ == "__main__":
+    files_start_deep = this_the_way()
+    print(files_start_deep)
