b69b8afc408ff58acef86f139580a2da0b279a65
daniel or abayev <dan5204@gmail.com>
2022-03-26 23:47:07 +0300

    With using of itertool.chain instead of for loop

--- cup_of_join.py
+++ cup_of_join.py
@@ -1,17 +1,19 @@
+from itertools import chain
+
+
 def join(*args: list[list[any]], sep: chr = '-') -> list[str]:
     """
     This function receive list of lists and make it one list separate by the sep char.
-    :param args: the list of lists.
-    :param sep: the char separate between the items, the default sign is '-'.
-    :return: the new list.
+    :param args: The list of lists.
+    :param sep: The char separate between the items, the default sign is '-'.
+    :return: The new list.
     """
     if not args:
         return None
     new_list = []
     for item in args:
-        for inner_item in item:
-            new_list += [inner_item]
-        new_list += [sep]
+        new_list = list(chain(new_list, item))
+        new_list = list(chain(new_list, sep))
     new_list = new_list[:-1]
     return new_list
 
