b92f68a1ac2624d9f773ae1661510a6fd15bd19e
daniel or abayev <dan5204@gmail.com>
2022-03-26 23:56:38 +0300

    Add main check, type hints and return type

--- piece_of_cake.py
+++ piece_of_cake.py
@@ -1,6 +1,6 @@
 # get the prices of the ingredients ,the amount need and the optional - which is the ingredients we don't need
 # and calculate the price.
-def get_recipe_price(prices, optionals=[], **ingredients):
+def get_recipe_price(prices: map, optionals: list = [], **ingredients: map) -> int:
     total_price = 0
     for ingredient in ingredients:
         ingredient_amount = int(ingredients[ingredient]) / 100
@@ -11,6 +11,7 @@ def get_recipe_price(prices, optionals=[], **ingredients):
     print(total_price)
 
 
-get_recipe_price({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100)
-get_recipe_price({'chocolate': 18, 'milk': 8}, optionals=['milk'], chocolate=300)
-get_recipe_price({})
+if __name__ == '__main__':
+    get_recipe_price({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100)
+    get_recipe_price({'chocolate': 18, 'milk': 8}, optionals=['milk'], chocolate=300)
+    get_recipe_price({})
