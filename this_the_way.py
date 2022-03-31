310abce58a7aed00b0849f8f76a898d4241f0314
daniel or abayev <dan5204@gmail.com>
2022-03-31 10:14:50 +0300

    Using one line function.

--- this_the_way.py
+++ this_the_way.py
@@ -1,18 +1,16 @@
 import os
 
 
-def this_the_way() -> list[str]:
+def this_the_way(path: str) -> list[str]:
     """
     This function get from the user path to folder and return all the files start with the word "deep".
-    :return: list of the files start with deep.
+    :param path: The path to the folder to check.
+    :return: List of the files start with deep.
     """
-    # the function read the path and create list of the files start with deep, then print them
-    folder_path = input("insert the path to the folder")
-    directory_list = os.listdir(folder_path)
-    start_with_deep_files = [file_names for file_names in directory_list if file_names.startswith("deep")]
-    return start_with_deep_files
+    return [file_names for file_names in os.listdir(path) if file_names.startswith("deep")]
 
 
 if __name__ == "__main__":
-    files_start_deep = this_the_way()
+    folder_path = input("insert the path to the folder")
+    files_start_deep = this_the_way(folder_path)
     print(files_start_deep)
